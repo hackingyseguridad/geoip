8fc50a1719a44735ce8cfb3682bf811b1a1278d6
hackingyseguridad <antonio_taboada@hotmail.es>
2019-08-28 14:01:31 +0200

    Update geo1000ip.sh

--- geo1000ip.sh
+++ geo1000ip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Geolocaliza 1000 IP en archivo ip.txt"
 echo
-for n in `cat ip.txt`; do echo $n ;curl -s http://ip-api.com/csv/$n
-done
+for n in `cat ip.txt`; do echo $n ;curl -s http://ip-api.com/csv/$n; done
